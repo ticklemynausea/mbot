2b2f3c9fbf667f6514682941963fbc6348462c87
ticklemynausea <emailoftheyear@gmail.com>
2014-07-28 22:12:24 +0100

    Fixed small derp.
    Mixed up \x03 \with \003.

--- mod/urlsniffer.cpp
+++ mod/urlsniffer.cpp
@@ -48,7 +48,7 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #define TIME_BUFFER_SIZE 80
 
 // used to prevent nick highlights
-#define BLOCK_CHAR "\x0030\x003"
+#define BLOCK_CHAR "\x030\x03"
 
 // number of seconds it takes before an old url is announced to the channel as old
 #define TIMEIN_OLD 0
